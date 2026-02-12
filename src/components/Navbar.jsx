141d72f4a9488427ae58af9fc02ff31ce3c74308
Aryaardhra <ardhra455@gmail.com>
2026-02-12 21:51:20 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -86,7 +86,7 @@ const Navbar = () => {
                         user ? (
                             <UserButton>
                                 <UserButton.MenuItems>
-                                    <UserButton.Action  label="My Bookings" labelIcon={<BookIcon/>} onClick={() => navigate("/my-booking")}/>
+                                    <UserButton.Action  label="My Bookings" labelIcon={<BookIcon/>} onClick={() => navigate("/bookings")}/>
                                 </UserButton.MenuItems>
                             </UserButton>
                         ) : (
